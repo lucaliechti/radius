9116c8f4ad9ff6ae6f344aa2c61d13572379cffc
Luca Liechti <luca.liechti@gmail.com>
2020-05-16 11:27:49 +0200

    Going back to tomcat; configuring new email host

--- src/main/java/radius/config/EmailConfig.java
+++ src/main/java/radius/config/EmailConfig.java
@@ -58,8 +58,8 @@ public class EmailConfig {
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
-        props.put("mail.ssl.enable", "true");
-        props.put("mail.smtp.starttls.enable", "false");
+        props.put("mail.ssl.enable", "false");
+        props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.debug", "false");
         props.put("mail.smtp.socketFactory.port", port);
         props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
