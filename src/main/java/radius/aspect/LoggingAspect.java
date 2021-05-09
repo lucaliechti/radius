72b24b663377c2785cae6cf7cf3c90ece1a13adc
Luca Liechti <Luca.Liechti@netcetera.com>
2021-05-09 21:17:26 +0200

    Logging instead of printing

--- src/main/java/radius/aspect/LoggingAspect.java
+++ src/main/java/radius/aspect/LoggingAspect.java
@@ -1,17 +1,19 @@
 package radius.aspect;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Aspect
 @Component
 public class LoggingAspect {
 
     @AfterReturning(pointcut="execution(* radius.web.controller.*.* (..))", returning="result")
     public void afterReturning(JoinPoint joinPoint, Object result) {
-        System.out.println(joinPoint.getSignature().getName() + " was called. Return value: " + result);
+        log.debug(joinPoint.getSignature().getName() + " was called. Return value: " + result);
     }
 
 }
